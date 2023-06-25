1e53a6b6181dab3708ddafe4b69d852f9d59d48e
Sikriet <matthgamez@gmail.com>
2023-06-25 05:36:21 -0400

    Primer commit

---
+++ config/axios.js
@@ -0,0 +1,14 @@
+// Cliente de Axios para consultar la API.
+
+import axios from "axios";
+
+const clienteAxios = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    },
+    withCredentials: true
+})
+
+export default clienteAxios
