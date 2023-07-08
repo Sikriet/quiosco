79221895d4afa9a6e1ba4106302225c483feb56f
Sikriet <matthgamez@gmail.com>
2023-07-08 13:41:39 -0400

    Arreglo de errores y ventana emergente responsiva

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,120 +1,125 @@
-import { useState, useEffect } from 'react'
-import useQuiosco from "../hooks/useQuiosco"
+import { useState, useEffect } from "react";
+import useQuiosco from "../hooks/useQuiosco";
 import { formatearDinero } from "../helpers";
 
 export default function ModalProducto() {
+  const { producto, handleClickModal, handleAgregarPedido, pedido } =
+    useQuiosco();
+  const [cantidad, setCantidad] = useState(1);
+  const [edicion, setEdicion] = useState(false);
 
-    const { producto, handleClickModal, handleAgregarPedido, pedido } = useQuiosco();
-    const [ cantidad, setCantidad ] = useState(1)
-    const [ edicion, setEdicion ] = useState(false)
+  // useEffect siempre tendrá un callback
+  useEffect(() => {
+    if (pedido.some((pedidoState) => pedidoState.id === producto.id)) {
+      const productoEdicion = pedido.filter(
+        (pedidoState) => pedidoState.id === producto.id
+      )[0];
+      setCantidad(productoEdicion.cantidad);
+      setEdicion(true);
+    }
+  }, [pedido]); // Arreglo de dependencias (Opcionales). Cada vez que "pedido" cambie, se vuelve a ejecutar
 
-    // useEffect siempre tendrá un callback
-    useEffect(() => {
-        if(pedido.some( pedidoState => pedidoState.id === producto.id )) {
-            const productoEdicion = pedido.filter( pedidoState => pedidoState.id === producto.id )[0]
-            setCantidad(productoEdicion.cantidad)
-            setEdicion(true)
-        }
-    }, [pedido]) // Arreglo de dependencias (Opcionales). Cada vez que "pedido" cambie, se vuelve a ejecutar
+  return (
+    <div className="md:flex gap-10 relative">
+      <div className="md:w-1/3">
+        <img
+          src={`/img/${producto.imagen}.jpg`}
+          alt={`Imagen producto ${producto.nombre}`}
+        />
+      </div>
 
-    return (
-      <div className="md:flex gap-10">
-        <div className="md:w-1/3">
-          <img
-            src={`/img/${producto.imagen}.jpg`}
-            alt={`Imagen producto ${producto.nombre}`}
-          />
+      <div className="md:w-2/3">
+        <div className="absolute top-0 right-0 mt-2 mr-2">
+          <button
+            onClick={handleClickModal}
+            className="flex bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded w-full md:w-auto md:mt-0 md:mr-0"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
         </div>
 
-        <div className="md:w-2/3">
-          <div className="flex justify-end">
-            <button onClick={handleClickModal}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </button>
-          </div>
+        <h1 className="text-3xl font-bold mt-5">{producto.nombre}</h1>
 
-          <h1 className="text-3xl font-bold mt-5">{producto.nombre}</h1>
+        <p className="mt-5 font-black text-5xl text-amber-500">
+          {formatearDinero(producto.precio)}
+        </p>
 
-          <p className="mt-5 font-black text-5xl text-amber-500">
-            {formatearDinero(producto.precio)}
-          </p>
-
-          <div className="flex gap-4 mt-5">
-            <button
-                type='button'
-                onClick={() => {
-                    if(cantidad <= 1) return
-                    setCantidad(cantidad - 1);
-                }}
+        <div className="flex gap-4 mt-5">
+          <button
+            type="button"
+            onClick={() => {
+              if (cantidad <= 1) return;
+              setCantidad(cantidad - 1);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </button>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
 
-            <p className="text-3xl">{cantidad}</p>
-
-            <button
-                type='button'
-                onClick={() => {
-                    if(cantidad >= producto.stock) return
-                    setCantidad(cantidad + 1);
-                }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </button>
-          </div>
+          <p className="text-3xl">{cantidad}</p>
 
           <button
             type="button"
-            className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
-            onClick={() => { 
-              // Los 3 puntos son para que todo (En este caso "Cantidad") se agrege a un solo objeto
-              handleAgregarPedido({...producto, cantidad})
-              // Para que luego de agregar un pedido se cierre la ventana automaticamente
-              handleClickModal()
-            }} 
+            onClick={() => {
+              if (cantidad >= producto.stock) return;
+              setCantidad(cantidad + 1);
+            }}
           >
-                {edicion ? 'Guardar Cambios' : 'Añadir al Pedido'}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
           </button>
         </div>
+
+        <button
+          type="button"
+          className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
+          onClick={() => {
+            // Los 3 puntos son para que todo (En este caso "Cantidad") se agrege a un solo objeto
+            handleAgregarPedido({ ...producto, cantidad });
+            // Para que luego de agregar un pedido se cierre la ventana automaticamente
+            handleClickModal();
+          }}
+        >
+          {edicion ? "Guardar Cambios" : "Añadir al Pedido"}
+        </button>
       </div>
-    );
+    </div>
+  );
 }
